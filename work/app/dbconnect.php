205163a19e2fe509d81abbb8534714d2a87b6a81
kentajinde <rr7i5fvv@icloud.com>
2021-02-23 12:38:07 +0900

    commit

--- work/app/dbconnect.php
+++ work/app/dbconnect.php
@@ -1,47 +1,27 @@
 <?php
 
 try{
+  // 開発環境
+  // $pdo = new PDO(
+  //   "mysql:host=db;dbname=base;charset=utf8mb4",
+  //   "base",
+  //   "base123",
+  //   [
+  //     PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+  //     PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+  //   ]
+  // );
+
+  // 本番環境
   $pdo = new PDO(
-    "mysql:host=db;dbname=base;charset=utf8mb4",
-    "base",
-    "base123",
+    "mysql:host=us-cdbr-east-03.cleardb.com;dbname=heroku_a04b3ce4b7ee565;charset=utf8mb4",
+    "b3e9847fb125f0",
+    "915d6388",
     [
       PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
       PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
     ]
   );
-
-  //ユーザーアカウント
-  // $pdo->query("DROP TABLE IF EXISTS members");
-  // $pdo->query(
-  //   "CREATE TABLE members(
-  //     id INT NOT NULL AUTO_INCREMENT,
-  //     name VARCHAR(255),
-  //     mail VARCHAR(255),
-  //     pass VARCHAR(100),
-  //     picture VARCHAR(255),
-  //     created DATETIME,
-  //     modified TIMESTAMP,
-  //     PRIMARY KEY(id)
-  //   )"
-  // );
-
-  //投稿内容
-  // $pdo->query("DROP TABLE IF EXISTS posts");
-  // $pdo->query(
-  //   "CREATE TABLE posts(
-  //     id INT NOT NULL AUTO_INCREMENT,
-  //     title VARCHAR(255),
-  //     author VARCHAR(30),
-  //     publish VARCHAR(255),
-  //     `desc` TEXT,
-  //     `rank` INT,
-  //     member_id INT,
-  //     created DATETIME,
-  //     modified TIMESTAMP,
-  //     PRIMARY KEY(id)
-  //   )"
-  // );
 }catch(PDOException $e){
   echo  $e->getMessage();
   exit();
